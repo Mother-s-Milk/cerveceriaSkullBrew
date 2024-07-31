b09759f33c53500f30b8e29f4da607f412772a02
Mother-s-Milk <francoveradrms@gmail.com>
2024-07-31 00:18:16 -0300

    Se agrega el Proyecto

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// let slider = document.getElementById('slider');
+
+// var imagenes = [];
+
+// imagenes[0] = 'nosotros/1.jpg';
+// imagenes[1] = 'nosotros/2.jpg';
+// imagenes[2] = 'nosotros/3.jpg';
+
+// var indice = 0;
+
+// let tiempo = 9000;
+
+// function carrusel () {
+//     slider = imagenes[indice];
+
+//     if (indice < 2) {
+//         indice++;
+//     }
+//     else {
+//         indice = 0;
+//     }
+// }
+
+// setInterval(carrusel, tiempo);
+
+// slider es el div contenedor
